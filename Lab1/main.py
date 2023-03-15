d64d88d9fb8679b50d5787652ff49800271c188c
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2023-03-15 23:52:52 +0100

    Task rearrange

--- Lab1/main.py
+++ Lab1/main.py
@@ -5,17 +5,32 @@ from dijkstra import get_shortest_path_dijkstra
 from astar import get_shortest_path_astar
 from results import print_results
 
-def main_task1(start_stop, end_stop, arrival_time):
+def task1a(start_stop, end_stop, arrival_time):
     setup_data(arrival_time)
     start_node = Node(start_stop, arrival_time)
-    
-    #cost, path = get_shortest_path_dijkstra(start_node, end_stop)
-    #print_results(start_stop, arrival_time, path, end_stop)
-    #print(f'Travel took {time / 60} minutes')
+
+    cost, path = get_shortest_path_dijkstra(start_node, end_stop)
+    print_results(start_stop, arrival_time, path, end_stop)
+    print(f'Cost: {cost}')
+
+
+def task1b(start_stop, end_stop, arrival_time):
+    setup_data(arrival_time)
+    start_node = Node(start_stop, arrival_time)
 
     cost, path = get_shortest_path_astar(start_node, end_stop, "t")
     print_results(start_stop, arrival_time, path, end_stop)
     print(f'Cost: {cost}')
 
+
+def task1c(start_stop, end_stop, arrival_time):
+    setup_data(arrival_time)
+    start_node = Node(start_stop, arrival_time)
+
+    cost, path = get_shortest_path_astar(start_node, end_stop, "s")
+    print_results(start_stop, arrival_time, path, end_stop)
+    print(f'Cost: {cost}')
+
+
 if __name__ == "__main__":
-    main_task1("żar", "kowale", datetime.strptime("9:00:00", "%H:%M:%S").time())
+    task1c("żar", "kowale", datetime.strptime("9:00:00", "%H:%M:%S").time())
